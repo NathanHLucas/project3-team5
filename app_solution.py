f7385e5dc8cf8ac050f6cc00c817bff5084ea5c7
nCervantez <nathaniel.cervantez@yahoo.com>
2023-09-29 18:06:32 -0500

    Added sqlite DB files. Succesfully pulled in the SQLite database files and returned them to the webpage in JSON format. Flask app routes updated to encompass all 3 stocks and their data.

--- app_solution.py
+++ app_solution.py
@@ -4,11 +4,32 @@ import flask
 #Create the app
 app = flask.Flask(__name__)
 
+#Import all the necessary data
+from fetch_data import blnk_data_dict
+from fetch_data import chpt_data_dict
+from fetch_data import evgo_data_dict
+
 #Define the home route
 @app.route("/")
 def index():
     #use render_template to render in our HTML file.
     return flask.render_template('index.html')
+
+# Define the get data route to serve data to the page
+@app.route("/blnk_data")
+def serveBLNKData():
+    #Return the data jsonify to access it
+    return flask.jsonify(blnk_data_dict)
+
+@app.route("/chpt_data")
+def serveCHPTData():
+    #Return the data jsonify to access it
+    return flask.jsonify(chpt_data_dict)
+
+@app.route("/evgo_data")
+def serveEVGOData():
+    #Return the data jsonify to access it
+    return flask.jsonify(evgo_data_dict)
 
 #Run the app
 if __name__ == '__main__':
---
+++ fetch_data.py
@@ -0,0 +1,97 @@
+#Import sqlalchemy dependencies
+from sqlalchemy import create_engine, text
+
+# SQLite database paths
+blnk_path = 'BLNK.db'
+chpt_path = 'CHPT.db'
+evgo_path = 'EVGO.db'
+
+# Create a SQLAlchemy engine.
+engine = create_engine(f'sqlite:///{blnk_path}')
+
+# Create a connection to the database.
+connection = engine.connect()
+
+# Defining the SQL query
+sql_query = text("SELECT * FROM data") 
+
+# Execute the query and fetch all the data.
+result = connection.execute(sql_query)
+#data contains all the info in the database
+blnk_data = result.fetchall()
+# Close the database connection when you're done.
+connection.close()
+
+#Convert the data into a dict form to allow it to be jsonify'd
+blnk_data_dict = [
+    {
+        'Date': row[0],
+        'Open': row[1],
+        'High': row[2],
+        'Low': row[3],
+        'Close': row[4],
+        'Adj Close': row[5],
+        'Volume': row[6]
+    }
+    for row in blnk_data
+]
+
+# Create a SQLAlchemy engine.
+engine = create_engine(f'sqlite:///{evgo_path}')
+
+# Create a connection to the database.
+connection = engine.connect()
+
+# Defining the SQL query
+sql_query = text("SELECT * FROM data") 
+
+# Execute the query and fetch all the data.
+result = connection.execute(sql_query)
+#data contains all the info in the database
+evgo_data = result.fetchall()
+# Close the database connection when you're done.
+connection.close()
+
+#Convert the data into a dict form to allow it to be jsonify'd
+evgo_data_dict = [
+    {
+        'Date': row[0],
+        'Open': row[1],
+        'High': row[2],
+        'Low': row[3],
+        'Close': row[4],
+        'Adj Close': row[5],
+        'Volume': row[6]
+    }
+    for row in evgo_data
+]
+
+# Create a SQLAlchemy engine.
+engine = create_engine(f'sqlite:///{chpt_path}')
+
+# Create a connection to the database.
+connection = engine.connect()
+
+# Defining the SQL query
+sql_query = text("SELECT * FROM data") 
+
+# Execute the query and fetch all the data.
+result = connection.execute(sql_query)
+#data contains all the info in the database
+chpt_data = result.fetchall()
+# Close the database connection when you're done.
+connection.close()
+
+#Convert the data into a dict form to allow it to be jsonify'd
+chpt_data_dict = [
+    {
+        'Date': row[0],
+        'Open': row[1],
+        'High': row[2],
+        'Low': row[3],
+        'Close': row[4],
+        'Adj Close': row[5],
+        'Volume': row[6]
+    }
+    for row in chpt_data
+]
