74283a4d3d4c7f6c0c4f840a0639306fe646a9b1
“angel <angelnekegrayson@Angelnekes-Air.lan>
2023-10-04 21:32:45 -0400

    chart code

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,5 +23,50 @@ d3.json(dataset)
   // Now you can log or do something with the data inside this callback
   console.log(stock_dates);
   console.log(stock_open);
+  let stockData = data.map(function (item) {
+    return {
+      dates: item.Date,
+      Open: parseFloat(item.Open),
+      High: parseFloat(item.High),
+      Low: parseFloat(item.Low),
+      Close: parseFloat(item.Close),
+    };
+  });
+
+  updateChart(stockData);
 });
 }
+
+function updateChart(data) {
+let dates = data.map(function (item) {
+return item.dates;
+});
+// chart for closing price overtime
+let trace = {
+x: dates,
+open: data.map(item => item.Open),
+high: data.map(item => item.High),
+low: data.map(item => item.Low),
+close: data.map(item => item.Close),
+increasing: { line: { color: 'black' } },
+decreasing: { line: { color: 'red' } },
+type: 'candlestick',
+xaxis: 'x',
+yaxis: 'y'
+};
+
+let plot_data = [trace];
+
+let layout = {
+dragmode: 'zoom',
+showlegend: false,
+xaxis: {
+  rangeslider: {
+    visible: false
+  }
+}
+};
+
+Plotly.newPlot('section3', plot_data, layout);
+}
+      
